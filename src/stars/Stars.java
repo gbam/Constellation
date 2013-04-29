aa6eed024f94b885b60ae19740de3634700b1b35
Phil Morley <runninggee57@gmail.com>
2013-04-28 19:38:56 -0500

    Greg's stuff written during regatta

--- src/stars/Stars.java
+++ src/stars/Stars.java
@@ -32,7 +32,8 @@ public class Stars {
 		pixelTotal = width * height;
 
 		Map<Long, Node> nodes = new HashMap<Long, Node>();
-
+		Queue<Edge> edges = new PriorityQueue<Edge>();
+		
 		//Iterate through buffered image
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {				
@@ -43,12 +44,30 @@ public class Stars {
 					//Hashmap (16 bits: x, 16 bits: y)
 					nodes.put((((long) x) << 16 + ((long) y)), n);
 				}
-				
-				if(x == width - 1){
-					
+				if(y==0 && x+1 != width){
+				  //This could be moved in the previous if block
+				  //The first row will not have been created vs all other rows would have been.
+				  
+				  //This will be used to make the edge nodes
+				  Node m = nodes.find((x << 16) + y);
+				  
+				  n = new Node(x, y+1, img.getRGB(x, y+1));
+				  nodes.put((((long) x) << 16 + ((long) y+1)), n)
+				  //Create relationships between nodes here.
+				  n = new Node(x+1, y, img.getRGB(x+1, y));
+				  nodes.put((((long) x+1) << 16 + ((long) y)), n)
 				}
-				if(y == height - 1){
-					
+				else if(y -1 != height){
+				  Node current  = nodes.find((x << 16) + y);
+				  Node right = nodes.find(((x+1) << 16) + y);
+				  Node down = new Node(x, y+1, img.getRGB(x, y+1));
+				  //Add edges here to connect nodes
+				}
+				else{
+				  //We now know this has to be the bottom row
+				  Node current  = nodes.find((x << 16) + y);
+				  //Create relationships between current 
+				  Node right = nodes.find(((x+1) << 16) + y);
 				}
 
 
