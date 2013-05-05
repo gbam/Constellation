041a22c47f3691a6dd664ac49cab7f6d369dcc84
Phil Morley <runninggee57@gmail.com>
2013-05-05 12:03:49 -0500

    Add hashCode and equal to Node to help equals function

--- src/stars/Node.java
+++ src/stars/Node.java
@@ -26,4 +26,40 @@ public class Node {
   public static long makeCoordinate(int x, int y) {
     return (((long) x) << 16) + ((long) y);
   }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + x;
+    result = prime * result + y;
+    return result;
+  }
+
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof Node)) {
+      return false;
+    }
+    Node other = (Node) obj;
+    if (x != other.x) {
+      return false;
+    }
+    if (y != other.y) {
+      return false;
+    }
+    return true;
+  }
 }
